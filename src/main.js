6b939b222e3fec0b3b041ce0ff7bb1db74247845
arock <arock392@aol.com>
2017-03-27 19:14:09 -0400

    add react

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Counter from './Counter';
  
+import LoginControl from './LoginControl';
+ 
 document.addEventListener('DOMContentLoaded', function() {
   ReactDOM.render(
     React.createElement(Counter),
